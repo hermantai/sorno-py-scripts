4eabf2d65d955703c6e737193dc52c064262741c
Herman Tai <htaihm@gmail.com>
2015-11-17 21:26:20 -0800

    Make sorno_gdoc.py accepts subcommands, add view_html
    Summary:
    Make sorno_gdoc.py accepts subcommands, so the default action "search" has to
    be explicitly stated as the subcommand "search" from now on.
    
    Add "view_html" subcommand to make printing of specific headers and sections
    of a Google doc given in a html form easier. Users should download the
    corresponding doc in html format to use "view_html".
    
    Test Plan:
    python scripts/sorno_gdoc.py view_html --header-tag h3 <path-to-html-file-google-doc> --section-for-header-regex <a-regex>

--- scripts/sorno_gdoc.py
+++ scripts/sorno_gdoc.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 """A command line client for Google Docs.
 
 The API doc used to implement this is in
@@ -31,8 +31,11 @@ API needed for this script is "Drive API" with the scope
 import argparse
 import os
 import pprint
+import re
 import subprocess
 import sys
+
+from lxml import html
 
 import apiclient
 from apiclient.discovery import build
@@ -50,154 +53,179 @@ REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob'
 CREDENTIALS_FILE = os.path.expanduser("~/.sorno_gdoc-google-drive-api.cred")
 
 
-def _main():
-    args = _parse_args(sys.argv[1:])
+class App(object):
+    def search_action(self, args):
+        query = args.query
+        output_file = args.output
 
-    query = args.query
-    output_file = args.output
+        # Copy your credentials from the console
+        client_id = os.getenv('GOOGLE_APP_PROJECT_CLIENT_ID')
+        client_secret = os.getenv('GOOGLE_APP_PROJECT_CLIENT_SECRET')
 
-    # Copy your credentials from the console
-    client_id = os.getenv('GOOGLE_APP_PROJECT_CLIENT_ID')
-    client_secret = os.getenv('GOOGLE_APP_PROJECT_CLIENT_SECRET')
+        if not client_id:
+            print(
+                "Please set the environment variable"
+                " GOOGLE_APP_PROJECT_CLIENT_ID")
+            sys.exit(1)
 
-    if not client_id:
-        print(
-            "Please set the environment variable"
-            " GOOGLE_APP_PROJECT_CLIENT_ID")
-        sys.exit(1)
-
-    if not client_secret:
-        print(
-            "Please set the environment variable"
-            " GOOGLE_APP_PROJECT_CLIENT_SECRET"
-        )
-        sys.exit(1)
-
-    # Run through the OAuth flow and retrieve credentials
-    flow = OAuth2WebServerFlow(client_id, client_secret, OAUTH_SCOPE,
-                               redirect_uri=REDIRECT_URI)
-
-    # Indicate we need the user to grant us permissions and give the auth code or
-    # not
-    need_get_code = True
-
-    if os.path.exists(CREDENTIALS_FILE) and args.use_credentials_cache:
-        storage = Storage(CREDENTIALS_FILE)
-        credentials = storage.get()
-        print("Use old credentials")
-        need_get_code = False
-
-    if need_get_code:
-        authorize_url = flow.step1_get_authorize_url()
-        print 'Go to the following link in your browser: ' + authorize_url
-        code = raw_input('Enter verification code: ').strip()
-
-        credentials = flow.step2_exchange(code)
-
-        storage = Storage(CREDENTIALS_FILE)
-        storage.put(credentials)
-
-    # Create an httplib2.Http object and authorize it with our credentials
-    http = httplib2.Http()
-    http = credentials.authorize(http)
-
-    drive_service = build('drive', 'v2', http=http)
-
-    results = retrieve_all_files(drive_service, query)
-
-    if not results:
-        print("No results")
-        sys.exit(1)
-    elif len(results) != 1:
-        for i, res in enumerate(results):
-            print("%d: %s" % (i, res['title']))
-
-        if args.lucky:
-            chosen = 0
-        elif args.lucky_number:
-            chosen = int(args.lucky_number) - 1  # from 1-based to 0-based
-        else:
-            chosen = int(raw_input("Choose file: "))
-
-        chosen_file = results[chosen]
-        print("Chosen: %s" % chosen_file['title'])
-    else:
-        chosen_file = results[0]
-
-    pprint.pprint(chosen_file)
-
-    print("File path: ")
-    path = []
-    parents = chosen_file['parents']
-    while parents:
-        parent = parents[0]
-        parent_file = drive_service.files().get(fileId=parent['id']).execute()
-        path.append(parent_file['title'])
-        parents = parent_file['parents']
-
-    path.reverse()
-    path.append(chosen_file['title'])
-
-    print(" > ".join(path))
-
-    if args.metadata_only:
-        return
-
-    links = chosen_file.get('exportLinks')
-
-    if not links or "text/plain" not in links:
-        print("No text/plain links available")
-        sys.exit(1)
-
-    content = download_file(drive_service, links['text/plain'])
-
-    if output_file:
-        with open(output_file, "w") as f:
-            f.write(content)
-        print("The file content is written in [%s]" % output_file)
-
-        if args.open_with:
-            subprocess.check_call(
-                args.open_with + " " + output_file,
-                shell=True,
+        if not client_secret:
+            print(
+                "Please set the environment variable"
+                " GOOGLE_APP_PROJECT_CLIENT_SECRET"
             )
-    else:
-        if args.open_with:
-            p = subprocess.Popen(args.open_with, stdin=subprocess.PIPE, shell=True)
-            p.communicate(content)
-            p.wait()
+            sys.exit(1)
+
+        # Run through the OAuth flow and retrieve credentials
+        flow = OAuth2WebServerFlow(client_id, client_secret, OAUTH_SCOPE,
+                                   redirect_uri=REDIRECT_URI)
+
+        # Indicate we need the user to grant us permissions and give the auth code or
+        # not
+        need_get_code = True
+
+        if os.path.exists(CREDENTIALS_FILE) and args.use_credentials_cache:
+            storage = Storage(CREDENTIALS_FILE)
+            credentials = storage.get()
+            print("Use old credentials")
+            need_get_code = False
+
+        if need_get_code:
+            authorize_url = flow.step1_get_authorize_url()
+            print 'Go to the following link in your browser: ' + authorize_url
+            code = raw_input('Enter verification code: ').strip()
+
+            credentials = flow.step2_exchange(code)
+
+            storage = Storage(CREDENTIALS_FILE)
+            storage.put(credentials)
+
+        # Create an httplib2.Http object and authorize it with our credentials
+        http = httplib2.Http()
+        http = credentials.authorize(http)
+
+        drive_service = build('drive', 'v2', http=http)
+
+        results = retrieve_all_files(drive_service, query)
+
+        if not results:
+            print("No results")
+            sys.exit(1)
+        elif len(results) != 1:
+            for i, res in enumerate(results):
+                print("%d: %s" % (i, res['title']))
+
+            if args.lucky:
+                chosen = 0
+            elif args.lucky_number:
+                chosen = int(args.lucky_number) - 1  # from 1-based to 0-based
+            else:
+                chosen = int(raw_input("Choose file: "))
+
+            chosen_file = results[chosen]
+            print("Chosen: %s" % chosen_file['title'])
         else:
-            print(content)
+            chosen_file = results[0]
+
+        pprint.pprint(chosen_file)
+
+        print("File path: ")
+        path = []
+        parents = chosen_file['parents']
+        while parents:
+            parent = parents[0]
+            parent_file = drive_service.files().get(fileId=parent['id']).execute()
+            path.append(parent_file['title'])
+            parents = parent_file['parents']
+
+        path.reverse()
+        path.append(chosen_file['title'])
+
+        print(" > ".join(path))
+
+        if args.metadata_only:
+            return
+
+        links = chosen_file.get('exportLinks')
+
+        if not links or "text/plain" not in links:
+            print("No text/plain links available")
+            sys.exit(1)
+
+        content = download_file(drive_service, links['text/plain'])
+
+        if output_file:
+            with open(output_file, "w") as f:
+                f.write(content)
+            print("The file content is written in [%s]" % output_file)
+
+            if args.open_with:
+                subprocess.check_call(
+                    args.open_with + " " + output_file,
+                    shell=True,
+                )
+        else:
+            if args.open_with:
+                p = subprocess.Popen(args.open_with, stdin=subprocess.PIPE, shell=True)
+                p.communicate(content)
+                p.wait()
+            else:
+                print(content)
+
+    def view_html_action(self, args):
+        with open(args.file) as f:
+            dom = html.fromstring(f.read())
+            sections = self._get_sections(dom)
+
+            for section in sections:
+                header = section['header']
+                if args.header_tag:
+                    if section['tag'] in args.header_tag:
+                        print(header.encode("utf8"))
+                else:
+                    print(header.encode("utf8"))
+
+                if args.section_for_header_regex:
+                    for header_regex in args.section_for_header_regex:
+                        if re.match(header_regex, header, re.I):
+                            for node in section['body']:
+                                print(node.text_content())
+                        break
+                else:
+                    for node in section['body']:
+                        print(node.text_content())
+        return 0
+
+    def _get_sections(self, dom):
+        sections = []
+        current_section = None
+        header_tags = ["h%d" % i for i in range(1, 7)]
+        body = dom.xpath("//body")[0]
+        for node in body:
+            if node.tag in header_tags and node.text_content():
+                current_section = []
+                sections.append(
+                    {
+                        'header': node.text_content(),
+                        'tag': node.tag,
+                        'body': current_section,
+                    }
+                )
+            elif current_section is not None:
+                current_section.append(node)
+        return sections
 
 
-def _parse_args(cmd_args):
+def main():
+    app = App()
+    args = parse_args(app, sys.argv[1:])
+    args.func(args)
+
+
+def parse_args(app_obj, cmd_args):
     description = __doc__.split("Copyright 2014")[0].strip()
     parser = argparse.ArgumentParser(
         description=description,
         formatter_class=argparse.RawDescriptionHelpFormatter,
-    )
-
-    parser.add_argument(
-        "query",
-        help="The search query, which only applies on the titles of the files",
-    )
-
-    parser.add_argument(
-        "--lucky",
-        action="store_true",
-        help="Choose the first result from the query without prompting",
-    )
-
-    parser.add_argument(
-        "--lucky-number",
-        help="Choose the lucky-number'th result instead of the first without"
-            " prompting",
-    )
-
-    parser.add_argument(
-        "--metadata-only",
-        action="store_true",
-        help="Only show the metadata of the file",
     )
 
     parser.add_argument(
@@ -210,7 +238,37 @@ def _parse_args(cmd_args):
             " time you use this script.",
     )
 
-    parser.add_argument(
+    subparsers = parser.add_subparsers(title="Subcommands")
+
+    parser_search = subparsers.add_parser(
+        "search",
+        help="Search a doc",
+        description="Search a doc")
+
+    parser_search.add_argument(
+        "query",
+        help="The search query, which only applies on the titles of the files",
+    )
+
+    parser_search.add_argument(
+        "--lucky",
+        action="store_true",
+        help="Choose the first result from the query without prompting",
+    )
+
+    parser_search.add_argument(
+        "--lucky-number",
+        help="Choose the lucky-number'th result instead of the first without"
+            " prompting",
+    )
+
+    parser_search.add_argument(
+        "--metadata-only",
+        action="store_true",
+        help="Only show the metadata of the file",
+    )
+
+    parser_search.add_argument(
         "--open-with",
         help="This option can be used to run the command"
         " specified with the filepath being the first command line argument"
@@ -221,11 +279,38 @@ def _parse_args(cmd_args):
         " Some useful commands for this option are less, sed, cat, etc."
     )
 
-    parser.add_argument(
+    parser_search.add_argument(
         "--output",
         help="Output to a file instead of printing the content of the file"
             "out",
     )
+
+    parser_search.set_defaults(func=app_obj.search_action)
+
+    view_html_help = (
+        "View a Google doc html file in a way that you can filter"
+        " information easily"
+    )
+    parser_view_html = subparsers.add_parser(
+        "view_html",
+        description=view_html_help,
+        help=view_html_help,
+    )
+    parser_view_html.add_argument(
+        "--header-tag",
+        help="HTML header tags to print out. Print out every tag if not"
+        " specified.",
+        action="append",
+    )
+    parser_view_html.add_argument(
+        "--section-for-header-regex",
+        help="The portion of text to print out following the header that"
+        " matches one of the regex specified with this option. Default to"
+        " print out everything",
+        action="append",
+    )
+    parser_view_html.add_argument("file")
+    parser_view_html.set_defaults(func=app_obj.view_html_action)
 
     args = parser.parse_args(cmd_args)
     return args
@@ -278,5 +363,5 @@ def retrieve_all_files(service, query):
 
 
 if __name__ == '__main__':
-    _main()
+    main()
 
