783af40959887112757ff8d52d68cc62d864a043
Herman Tai <htaihm@gmail.com>
2020-05-11 20:56:27 -0700

    bump the version number

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ with open("README.rst", "r") as f:
 
 setup(
     name="sorno-py-scripts",
-    version="0.48.1",
+    version="0.48.2",
     description="Herman Tai's python scripts all prefixed with \"sorno_\"",
     long_description=readme_text,
     author="Herman Tai",
