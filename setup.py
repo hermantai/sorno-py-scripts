955c5cb8e8a9e1ab721a7d7bd2fead9c5d333f4f
Herman Tai <htaihm@gmail.com>
2024-02-20 16:43:13 -0800

    Update version

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ with open("README.rst", "r") as f:
 
 setup(
     name="sorno-py-scripts",
-    version="0.48.7",
+    version="0.48.8",
     description="Herman Tai's python scripts all prefixed with \"sorno_\"",
     long_description=readme_text,
     author="Herman Tai",
